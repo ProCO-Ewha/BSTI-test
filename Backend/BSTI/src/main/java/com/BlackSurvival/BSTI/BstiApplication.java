2c76652088b45a5dcddf3c87d56966a4421ebb4f
syeon.kang <syeon.dev@gmail.com>
2024-01-07 20:54:23 +0900

    init: Create Spring Project

---
+++ Backend/BSTI/src/main/java/com/BlackSurvival/BSTI/BstiApplication.java
@@ -0,0 +1,13 @@
+package com.BlackSurvival.BSTI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BstiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BstiApplication.class, args);
+	}
+
+}
