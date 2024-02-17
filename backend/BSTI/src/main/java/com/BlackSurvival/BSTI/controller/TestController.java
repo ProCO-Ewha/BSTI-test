e94ed67f3f24757ff92e8df94a056cde816069f1
강승연 <syeon.dev@gmail.com>
2024-02-17 22:34:59 +0900

    test
    test

--- backend/BSTI/src/main/java/com/BlackSurvival/BSTI/controller/TestController.java
+++ backend/BSTI/src/main/java/com/BlackSurvival/BSTI/controller/TestController.java
@@ -3,7 +3,6 @@ package com.BlackSurvival.BSTI.controller;
 import com.BlackSurvival.BSTI.entity.Test;
 import com.BlackSurvival.BSTI.form.TestForm;
 import com.BlackSurvival.BSTI.service.TestService;
-import com.amazonaws.services.s3.AmazonS3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.net.URL;
 import java.util.Optional;
 
 @Controller
@@ -22,7 +20,6 @@ public class TestController {
 
 
     private final TestService service;
-    //private final AmazonS3 s3;
 
     @GetMapping("show/{id}")
     //@ResponseBody
@@ -31,9 +28,6 @@ public class TestController {
 
         if(testOpt.isPresent()){
             model.addAttribute("data", testOpt.get());
-            //URL url = s3.getUrl("bsti-imageserver", "1");
-            //String urlTest = ""+url;
-            //model.addAttribute("img1", urlTest);
         }
         else{
             model.addAttribute("data", new Test(id, "Why you are here?"));
