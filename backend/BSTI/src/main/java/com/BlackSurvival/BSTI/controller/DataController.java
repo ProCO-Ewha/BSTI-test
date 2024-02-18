95f21cfcb887b89a82551db403d6ec27aff4a368
강승연 <syeon.dev@gmail.com>
2024-02-18 17:57:25 +0900

    [UPDATE] 스프링 웹 문제(초기화면 없음) 해결

--- backend/BSTI/src/main/java/com/BlackSurvival/BSTI/controller/DataController.java
+++ backend/BSTI/src/main/java/com/BlackSurvival/BSTI/controller/DataController.java
@@ -2,21 +2,29 @@ package com.BlackSurvival.BSTI.controller;
 
 import com.BlackSurvival.BSTI.entity.Character;
 import com.BlackSurvival.BSTI.service.CharacterService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/crud")
 public class DataController {
 
     @Autowired
     CharacterService service;
+
+    @GetMapping()
+    public String show(){
+        return "crud";
+    }
 
     @PostMapping("/insert")
     public void insert(@Validated Character character, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes){
