1719fbd0e6608d6699c909436750be4104113518
강승연 <syeon.dev@gmail.com>
2024-02-19 13:58:04 +0900

    [UPDATE] JPA 데이터 @Entity 설정 오류

--- backend/BSTI/src/main/java/com/BlackSurvival/BSTI/entity/BSCharacter.java
+++ backend/BSTI/src/main/java/com/BlackSurvival/BSTI/entity/BSCharacter.java
@@ -1,11 +1,13 @@
 package com.BlackSurvival.BSTI.entity;
 
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class BSCharacter {
