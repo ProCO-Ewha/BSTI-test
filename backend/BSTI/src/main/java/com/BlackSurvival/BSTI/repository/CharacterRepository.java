80a26cb4ca9268ce29e7843a28612466161d94e8
강승연 <syeon.dev@gmail.com>
2024-02-19 13:45:17 +0900

    [REFACTOR] DB 연결방식 변경 JDBC -> JPA

--- backend/BSTI/src/main/java/com/BlackSurvival/BSTI/repository/CharacterRepository.java
+++ backend/BSTI/src/main/java/com/BlackSurvival/BSTI/repository/CharacterRepository.java
@@ -1,7 +1,8 @@
 package com.BlackSurvival.BSTI.repository;
 
 import com.BlackSurvival.BSTI.entity.BSCharacter;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CharacterRepository extends CrudRepository<BSCharacter, String> {
+public interface CharacterRepository extends JpaRepository<BSCharacter, String> {
 }
