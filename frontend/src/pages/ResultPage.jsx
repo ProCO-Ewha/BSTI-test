d4827e33a0d383048f54f97d3dadda9ac6cede9c
Sarang <hangpfm0518@ewhain.net>
2024-03-09 21:51:50 +0900

    Update axios code

--- frontend/src/pages/ResultPage.jsx
+++ frontend/src/pages/ResultPage.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import BtnComponent from '../components/BtnComponent';
-import CharacterImage from '../Half.png'; //예시이미지
+import axios from 'axios'; // axios 추가
 import '../styles/ResultPage.css';
 
 const ResultPage = () => {
   const [characterData, setCharacterData] = useState({
     name: '',
-    image: CharacterImage,
+    image_url: '',
     description: '',
     mainQuote: '',
   });
 
   useEffect(() => {
-    axios.get('서버 엔드포인트로 수정 부탁!')
+    axios.get('http://3.35.138.123:8080/') // AWS 서버의 엔드포인트로 요청을 보냄
       .then((response) => {
         console.log('성공적으로 데이터를 받아왔습니다.', response.data);
         setCharacterData(response.data);  // 받은 데이터로 state 업데이트
@@ -36,31 +36,30 @@ const ResultPage = () => {
 
   return (
     <div className="result-page-container">
-    <div className="all-contents">
-      <p className="yours">
-        당신과 어울리는 <br />블랙서바이벌 캐릭터는?
-      </p>
-      <div className="character-info">
-      <h2 className="character-name">{characterData.name}</h2>
-        <img src={characterData.image} alt={characterData.name} className="character-image" />
-        <div className="character-details">
-          <p className="main-quote" style={{ color: '#D9EFF6' }}>{characterData.mainQuote}</p>
-          <p className="character-description">{characterData.description}</p>
-        </div>
+      <div className="all-contents">
+        <p className="yours">
+          당신과 어울리는 <br />블랙서바이벌 캐릭터는?
+        </p>
+        <div className="character-info">
+          <h2 className="character-name">{characterData.name}</h2>
+          <img src={characterData.image_url} alt={characterData.name} className="character-image" />
+          <div className="character-details">
+            <p className="main-quote" style={{ color: '#D9EFF6' }}>{characterData.mainQuote}</p>
+            <p className="character-description">{characterData.description}</p>
+          </div>
 
-        <div className="btn-group">
+          <div className="btn-group">
             <Link to="/">
               <BtnComponent label="테스트 다시하기" />
             </Link>
             <div style={{ margin: '20px' }}></div>
             <BtnComponent label="테스트 공유하기" onClick={handleCopyLink} />
-        </div>
+          </div>
 
-      </div>
+        </div>
       </div>
     </div>
   );
 };
 
 export default ResultPage;
-
