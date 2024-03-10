83189bb33381632032bafddd42d658ae7ae086d4
강승연 <syeon.dev@gmail.com>
2024-03-10 20:29:45 +0900

    마지막!

--- frontend/src/pages/ResultPage.jsx
+++ frontend/src/pages/ResultPage.jsx
@@ -13,7 +13,7 @@ const ResultPage = () => {
   });
 
   useEffect(() => {
-    axios.get('http://3.35.138.123:8080/') // AWS 서버의 엔드포인트로 요청을 보냄
+    axios.get('http://3.35.138.123:8080/bsti/returnresult') // AWS 서버의 엔드포인트로 요청을 보냄
       .then((response) => {
         console.log('성공적으로 데이터를 받아왔습니다.', response.data);
         setCharacterData(response.data);  // 받은 데이터로 state 업데이트
