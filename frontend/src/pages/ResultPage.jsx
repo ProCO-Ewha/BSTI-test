2375fc17855513ea96e5bdf8edbb522a6a68e6be
Sarang <hangpfm0518@ewhain.net>
2024-03-10 20:42:35 +0900

    Modify innerHTML to Markdown

--- frontend/src/pages/ResultPage.jsx
+++ frontend/src/pages/ResultPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import BtnComponent from '../components/BtnComponent';
 import axios from 'axios';
+import ReactMarkdown from 'react-markdown'; // Import react-markdown
 import '../styles/ResultPage.css';
 
 const ResultPage = () => {
@@ -15,15 +16,15 @@ const ResultPage = () => {
   const { state } = useLocation();
 
   useEffect(() => {
-    axios.get('http://3.35.138.123:8080/bsti/returnresult') // AWS 서버의 엔드포인트로 요청을 보냄
+    axios.get('http://3.35.138.123:8080/bsti/returnresult')
       .then((response) => {
         console.log('성공적으로 데이터를 받아왔습니다.', response.data);
-        setCharacterData(response.data);  // 받은 데이터로 state 업데이트
+        setCharacterData(response.data);
       })
       .catch((error) => {
         console.error('데이터를 받아오는 중 에러가 발생했습니다.', error);
       });
-  }, []);  // 컴포넌트가 마운트될 때 한 번만 호출
+  }, []);
 
   const handleCopyLink = () => {
     const currentURL = window.location.href;
@@ -46,10 +47,9 @@ const ResultPage = () => {
           <h2 className="character-name">{characterData.name}</h2>
           <img src={characterData.image_url} alt={characterData.name} className="character-image" />
           <div className="character-details">
-          <p className="main-quote" style={{ color: '#D9EFF6' }}>{characterData.mainQuote}</p>
-          <p className="character-description" dangerouslySetInnerHTML={{ __html: characterData.description }} />
+            <p className="main-quote" style={{ color: '#D9EFF6' }}>{characterData.mainQuote}</p>
+            <ReactMarkdown className="character-description" source={characterData.description} />
           </div>
-
 
           <div className="btn-group">
             <Link to="/">
@@ -58,7 +58,6 @@ const ResultPage = () => {
             <div style={{ margin: '20px' }}></div>
             <BtnComponent label="테스트 공유하기" onClick={handleCopyLink} />
           </div>
-
         </div>
       </div>
     </div>
